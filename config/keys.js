40476002d1b72934724e5c4d67f5b4a05b28e460
Foysal Ahmed <foysalxahmed@gmail.com>
2021-04-08 17:32:42 +0600

    Initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+    mongoURI:
+        'mongodb+srv://foysal:3697@cluster0.58juz.mongodb.net/devconnector?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const users = require('./routes/api/users')
+const posts = require('./routes/api/posts')
+const profile = require('./routes/api/profile')
+
+const app = express()
+
+//DB Config
+const db = require('./config/keys').mongoURI
+
+//COnnect to mongoDB
+mongoose.connect(db)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log('MongoDB Connection Error'))
+
+
+app.get('/', (req, res) => res.send('<h1>Hello</h1>'))
+
+//Use Routes
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
