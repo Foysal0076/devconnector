b6432a0400028b3e2b85be45450b0ea59688e6f3
Foysal Ahmed <foysalxahmed@gmail.com>
2021-04-13 18:11:55 +0600

    Added  sign  up  and  login  REACT

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,45 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
+import jwt_decode from 'jwt-decode'
+import setAuthToken from './utils/setAuthToken'
+import { setCurrentUser } from './actions/authActions'
 
 import './App.css';
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Landing from './components/layout/Landing'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
+
+//Check for token
+if (localStorage.jwtToken) {
+  //set auth token header auth
+  setAuthToken(localStorage.jwtToken)
+
+  //Decode the token and get the user info and expiration
+  const decoded = jwt_decode(localStorage.jwtToken)
+
+  //set user and is Authenticated
+  store.dispatch(setCurrentUser(decoded))
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>My React App</h1>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <div className="container">
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </Provider>
+
   );
 }
 
