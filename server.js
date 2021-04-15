e4a007754a31a0b503e761786858bb6c06336f9b
Foysal Ahmed <foysalxahmed@gmail.com>
2021-04-16 01:41:05 +0600

    Prepared for deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const users = require('./routes/api/users')
 const posts = require('./routes/api/posts')
 const profile = require('./routes/api/profile')
+const path = require('path')
 // const bodyParser = require('body-parser')
 const passport = require('passport')
 
@@ -31,6 +32,16 @@ require('./config/passport')(passport)
 app.use('/api/users', users)
 app.use('/api/profile', profile)
 app.use('/api/posts', posts)
+
+//Server static folder if in production
+if (process.env.NODE_ENV == 'production') {
+    //Set Static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
